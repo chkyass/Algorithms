c3973ddaeaef527e90814288602e88c1d98f7659
rand0m <you@example.com>
2019-08-24 19:20:39 +0200

    test priority_queue with catch2

---
+++ test/test_priority_queue.cpp
@@ -0,0 +1,55 @@
+#define CATCH_CONFIG_MAIN 
+#include "include/catch.hh"
+#include "src/priority_queue.hpp"
+#include <string>
+#include <iostream>
+
+TEST_CASE( "Empty queue", "[priority queue]" ) 
+{
+    std::vector<int> a;
+    priority_queue<int> p(a);
+    REQUIRE(p.isEmpty() == true);
+}
+
+TEST_CASE( "Singleton", "[priority queue]" )
+{
+    std::vector<int> a{1};
+    priority_queue<int> p(a);
+    REQUIRE(p.isEmpty() == false);
+}
+
+TEST_CASE( "Test pop", "[priority queue]")
+{
+  std::vector<int> a{1,3,6,2};
+  priority_queue<int> p(a);
+  REQUIRE(p.pop() == 6);
+  REQUIRE(p.pop() == 3);
+}
+
+TEST_CASE( "Test add", "[priority queue]")
+{
+  std::vector<int> a{1};
+  priority_queue<int> p(a);
+  p.add(2);
+  p.add(3);
+  REQUIRE(p.top() == 3);
+}
+
+TEST_CASE( "Test top", "[priority queue]")
+{
+  std::vector<int> a{1,3,6,2};
+  priority_queue<int> p(a);
+  p.add(2);
+  p.add(6);
+  REQUIRE(p.top() == 6);
+  REQUIRE(p.top() == 6);
+
+}
+
+/*int main()
+{
+  std::vector<std::string> a{"a"};
+  priority_queue<int> p;
+  p.isEmpty();
+  return 0;
+}*/
