eca29b399a9268fd6736279cb350b846809c195f
rand0m <you@example.com>
2019-08-28 22:04:00 +0200

    refactor the binary search algorithm to be able to insert or retrieve a node using the same function

--- src/bst.hpp
+++ src/bst.hpp
@@ -14,50 +14,73 @@ class bst
     bst* left_;
     bst* right_;
     E key_;
-    void insertNode(bst*, E );
+    size_t size_ = 1;
+    bool insertNode(bst*, const E&, bst*);
 
   public:
-    bst(E e);
-    bool contain(E&);
+    bst(E e, bst *left=nullptr, bst *right=nullptr);
+    bool contain(const E&);
+    size_t size();
     virtual void insert(E);
     virtual ~bst();
     void operator=(bst) = delete;
     bst(bst&) = delete;
 };
 
-template <class E> bst<E>::bst(E k): key_(k)
+template <class E> bst<E>::bst(E k, bst* left, bst* right):left_(left), right_(right), key_(k)
 {}
 
 /*
-  If the element already exist does nothing
-  Insert element at a leaf maintaining the invariant
+  Implementation of the binary search
+  take the root node of the tree as first paramter
+  the key to insert
+  and node to insert
+  return true the new node is inserted
 */
-template <class E> void bst<E>::insert(E e)
-{
-  insertNode(this, e);
-}
-
-
-/*
-  find the place where the node should be and insert it
-  or does nothing if already present
-*/
-template <class E> void bst<E>::insertNode(bst *node, E e)
+template <class E> bool bst<E>::insertNode(bst *node, const E &e, bst *toInsert)
 {
   if(e < node->key_)
   {
     if(node->left_ == nullptr)
-      node->eft_ = new bst(e);
+    {
+      node->left_ = toInsert;
+      return true;
+    }
     else
-      insertNode(left_, e);
+      insertNode(left_, e, toInsert);
   }
   else if (e > node->key_)
   {
     if(node->right_ == nullptr)
-      node->right_.reset(new bst(e));
+    {
+      node->right_ = toInsert;
+      return true;
+    }
     else
-      insertNode(node->right_, e);
+      insertNode(node->right_, e, toInsert);
   }
+  return false;
+}
+
+/*
+  wrapper for insertNode
+*/
+template <class E> void bst<E>::insert(E e)
+{
+  bst* newNode = new bst(e);
+  // if the node wasn't inserted because already present free it
+  if(!insertNode(this, e, newNode))
+    delete newNode;
+  else
+    size_++;
+}
+
+/*
+  Use insertNode with a null node to detemine if the key is present
+*/
+template <class E> bool bst<E>::contain(const E &key)
+{
+  return !insertNode(this, key, nullptr);
 }
 
 /*
@@ -67,15 +90,19 @@ template <class E> bst<E>::~bst()
 {
   if(left_ != nullptr)
   {
-    left_.~bst();
+    left_->~bst();
     delete left_;
   }
   if(right_ != nullptr)
   {
-    right_.~bst();
+    right_->~bst();
     delete right_;
   }
-  
+}
+
+template<class E> size_t bst<E>::size()
+{
+  return size_;
 }
 
 #endif /* bst_hpp */
