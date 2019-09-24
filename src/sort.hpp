186b3cba22c8aaadf98e34d88fa9dfe635a8fbd5
rand0m <you@example.com>
2019-09-24 21:45:04 +0200

    implement quicksort

--- src/sort.hpp
+++ src/sort.hpp
@@ -1,5 +1,6 @@
 #ifndef sort_h
 #define sort_h
+#include <utility>
 
 template <class E>
 class sort 
@@ -7,13 +8,17 @@ class sort
   private:
     size_t treshold_;
     void mergeSortHelper(E*, E*, size_t, size_t);
+    void quickSortHelper(E*, size_t, size_t);
     void merge(E*, E*, size_t, size_t, size_t);
+    size_t partition(E*, size_t, size_t);
+    size_t middleOfThree(E*, size_t, size_t, size_t);
 
   public:
     sort(size_t treshold = 16): treshold_(treshold){}
     E* mergeSort(E*, size_t size);
     void raddixSort(E*);
     void insertionSort(E*, size_t, size_t=0);
+    void quickSort(E*, size_t);
 }; 
 
 template <class E> void sort<E>::merge(E* array, E* copy, size_t start, size_t middle ,size_t end)
@@ -44,13 +49,8 @@ template <class E> void sort<E>::merge(E* array, E* copy, size_t start, size_t m
   }
 }
 
-
 template <class E> void sort<E>::mergeSortHelper(E* array, E* copy, size_t start, size_t end)
 {
-  if(start >= end)
-  {
-    copy[start] = array[start];
-  }
   if(end > start)
   {
     size_t middle = (start+end)/2;
@@ -59,7 +59,6 @@ template <class E> void sort<E>::mergeSortHelper(E* array, E* copy, size_t start
     merge(array, copy, start, middle, end);
   }
 }
-
 
 template <class E> E* sort<E>::mergeSort(E *array, size_t size)
 {
@@ -70,11 +69,13 @@ template <class E> E* sort<E>::mergeSort(E *array, size_t size)
   }
   else {
     E* copy = new E[size];
+    std::copy(array, array+size, copy);
     size--;
     size_t middle = size/2;
     mergeSortHelper(copy, array, 0, middle);
     mergeSortHelper(copy, array, middle + 1, size);
     merge(array, copy, 0, middle, size);
+    std::copy(copy, copy + (++size), array);
     return copy;
   }
 }
@@ -90,6 +91,81 @@ template <class E> void sort<E>::insertionSort(E* array, size_t size, size_t sta
       std::swap(array[j-1], array[j]);
     }
   } 
+}
+
+template <class E> size_t sort<E>::middleOfThree(E* array , size_t a, size_t b, size_t c)
+{
+    if (array[a] > array[b])  
+    { 
+        if (array[b] > array[c]) 
+            return b; 
+        else if (array[a] > array[c]) 
+            return c; 
+        else
+            return a; 
+    }
+    else 
+    { 
+        if (array[a] > array[c]) 
+            return a; 
+        else if (array[b] > array[c]) 
+            return c; 
+        else
+            return b; 
+    }
+}
+
+
+template <class E> size_t sort<E>::partition(E* array, size_t start, size_t end)
+{
+  // choose pivot as median of three
+  if ((end - start + 1) > 2)
+  {
+    size_t middle = middleOfThree(array, start, (start+end)/2, end);
+    std::swap(array[middle], array[end]);
+  }
+
+  // move all smaller than pivot to left and bigger to right
+  // start at -1 in case all elements are bigger that pivot
+  size_t last_lower = start - 1;
+  for(size_t last_bigger = start; last_bigger < end; last_bigger++)
+  {
+    if(array[last_bigger] <= array[end])
+    {
+      last_lower++;
+      std::swap(array[last_bigger], array[last_lower]);
+    }
+  }
+
+  std::swap(array[++last_lower], array[end]);
+  return last_lower;
+}
+
+template <class E> void sort<E>::quickSortHelper(E* array, size_t start, size_t end)
+{
+  if(start < end)
+  {
+    size_t pivot =  partition(array, start, end);
+    // Avoid integer underflow
+    if(pivot - 1 < pivot)
+      quickSortHelper(array, start, pivot - 1);
+    quickSortHelper(array, pivot + 1, end);
+  }
+}
+
+template <class E> void sort<E>::quickSort(E* array, size_t size)
+{
+  treshold_ = 1;
+  if(size <= treshold_)
+  {
+    insertionSort(array, size);
+  }
+  else
+  {
+    size_t pivot = partition(array, 0, size - 1);
+    quickSortHelper(array, 0, pivot - 1);
+    quickSortHelper(array, pivot + 1, size - 1);
+  }
 }
 
 #endif
